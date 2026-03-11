ba5d6f92f4fb169d801ce9d9bac070ea823999a7
Oleg Milantiev <oleg@milantiev.com>
2026-03-11 08:33:19 +0300

    Remove id from frontend tables

--- develop/symfony/src/Application/DTO/VideoItemDTO.php
+++ develop/symfony/src/Application/DTO/VideoItemDTO.php
@@ -7,7 +7,6 @@ use App\Domain\Video\Entity\Video;
 readonly class VideoItemDTO
 {
     private function __construct(
-        public string $id,
         public string $title,
         public string $status,
         public string $createdAt
@@ -16,7 +15,6 @@ readonly class VideoItemDTO
     public static function fromDomain(Video $video): self
     {
         return new self(
-            id: $video->id()->toString(),
             title: $video->title()->value(),
             status: $video->status()->name,
             createdAt: $video->createdAt()->format('Y-m-d H:i')
