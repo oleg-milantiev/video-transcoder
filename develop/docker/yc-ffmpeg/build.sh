27c4b30249511b29a3b8db12df4203d1187dafdd
Oleg Milantiev <oleg@milantiev.com>
2026-03-20 00:28:36 +0300

    CI/CD - first steps

--- develop/docker/yc-ffmpeg/build.sh
+++ develop/docker/yc-ffmpeg/build.sh
@@ -2,5 +2,12 @@
 
 . ../../.env
 
-docker build --build-arg PROJECT_VERSION=${PROJECT_VERSION} -t olegmilantiev/yc-ffmpeg:${PROJECT_VERSION} . && \
+GIT_REPOSITORY_URL=${GIT_REPOSITORY_URL:-$(git -C ../.. config --get remote.origin.url)}
+
+if [ -z "$GIT_REPOSITORY_URL" ]; then
+    echo "GIT_REPOSITORY_URL is not set and remote.origin.url is empty"
+    exit 1
+fi
+
+docker build --build-arg PROJECT_VERSION=${PROJECT_VERSION} --build-arg GIT_REPOSITORY_URL=${GIT_REPOSITORY_URL} -t olegmilantiev/yc-ffmpeg:${PROJECT_VERSION} . && \
     docker push olegmilantiev/yc-ffmpeg:${PROJECT_VERSION}
--- develop/docker/yc-php/build.sh
+++ develop/docker/yc-php/build.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+set -eu
+
 . ../../.env
 
-docker build -t olegmilantiev/yc-php:${PROJECT_VERSION} . && docker push olegmilantiev/yc-php:${PROJECT_VERSION}
+GIT_REPOSITORY_URL=${GIT_REPOSITORY_URL:-$(git -C ../.. config --get remote.origin.url)}
+
+if [ -z "$GIT_REPOSITORY_URL" ]; then
+	echo "GIT_REPOSITORY_URL is not set and remote.origin.url is empty"
+	exit 1
+fi
+
+TMP_REPO_DIR="/tmp/yc-release-${PROJECT_VERSION}-$$"
+
+cleanup() {
+	rm -rf "$TMP_REPO_DIR"
+}
+
+trap cleanup EXIT INT TERM
+
+git clone --depth 1 --branch "release/${PROJECT_VERSION}" "$GIT_REPOSITORY_URL" "$TMP_REPO_DIR"
+
+docker build \
+	-t olegmilantiev/yc-php:${PROJECT_VERSION} \
+	-f "$TMP_REPO_DIR/develop/docker/yc-php/Dockerfile" \
+	"$TMP_REPO_DIR/develop" && docker push olegmilantiev/yc-php:${PROJECT_VERSION}
