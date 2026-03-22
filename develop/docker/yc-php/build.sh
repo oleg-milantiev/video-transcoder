8487fa87c8098d55ae7ce8f9b8c670fcbd0491df
Oleg Milantiev <oleg@milantiev.com>
2026-03-22 23:04:37 +0300

    Add ext-redis to php image

--- develop/docker/yc-php/build.sh
+++ develop/docker/yc-php/build.sh
@@ -22,6 +22,7 @@ trap cleanup EXIT INT TERM
 git clone --depth 1 --branch "release/${PROJECT_VERSION}" "$GIT_REPOSITORY_URL" "$TMP_REPO_DIR"
 
 docker build \
+	--build-arg DATABASE_URL="${DATABASE_URL}" \
 	--build-arg DEFAULT_URI="${DEFAULT_URI}" \
 	-t olegmilantiev/yc-php:${PROJECT_VERSION} \
 	-f "$TMP_REPO_DIR/develop/docker/yc-php/Dockerfile" \
