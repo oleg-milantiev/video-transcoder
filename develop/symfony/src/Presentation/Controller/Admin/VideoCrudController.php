d168301983ed27b0ff5b55a953381ec163da7495
Oleg Milantiev <oleg@milantiev.com>
2026-03-25 09:22:06 +0300

    Admin - Video - Link to tasks

--- develop/symfony/src/Presentation/Controller/Admin/VideoCrudController.php
+++ develop/symfony/src/Presentation/Controller/Admin/VideoCrudController.php
@@ -15,9 +15,15 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\DateTimeFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class VideoCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private readonly AdminUrlGenerator $adminUrlGenerator,
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return VideoEntity::class;
@@ -69,6 +75,55 @@ class VideoCrudController extends AbstractCrudController
                 ->onlyOnDetail(),
             DateTimeField::new('createdAt')->hideOnForm(),
             DateTimeField::new('updatedAt')->hideOnForm(),
+            AssociationField::new('tasks')
+                ->setTemplatePath('admin/field/video_tasks_summary.html.twig')
+                ->formatValue(fn ($value, ?VideoEntity $entity) => [
+                    'presets' => $this->collectPresetLinks($entity),
+                ])
+                ->hideOnForm(),
         ];
+    }
+
+    private function collectPresetLinks(?VideoEntity $video): array
+    {
+        if (null === $video) {
+            return [];
+        }
+
+        $presets = [];
+        foreach ($video->tasks as $task) {
+            $preset = $task->preset;
+            if (null !== $preset?->id && '' !== trim($preset->title)) {
+                $id = $preset->id->toRfc4122();
+                $presets[$id] = [
+                    'title' => $preset->title,
+                    'url' => $this->buildTasksUrl($video, $id),
+                ];
+            }
+        }
+
+        return array_values($presets);
+    }
+
+    private function buildTasksUrl(?VideoEntity $video, ?string $presetId = null): ?string
+    {
+        if (null === $video?->id) {
+            return null;
+        }
+
+        $urlGenerator = $this->adminUrlGenerator
+            ->unsetAll()
+            ->setController(TaskCrudController::class)
+            ->setAction(Crud::PAGE_INDEX)
+            ->set('filters[video][comparison]', '=')
+            ->set('filters[video][value]', $video->id->toRfc4122());
+
+//        if (null !== $presetId) {
+//            $urlGenerator
+//                ->set('filters[preset][comparison]', '=')
+//                ->set('filters[preset][value]', $presetId);
+//        }
+
+        return $urlGenerator->generateUrl();
     }
 }
