ba03a34ca1c4ff04626a19a96197f6f307f2f171
Oleg Milantiev <oleg@milantiev.com>
2026-03-28 19:11:12 +0300

    phpUnit xDebug generate html cover report

---
+++ develop/cover.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#docker exec -i develop-php-1 sh -c 'export XDEBUG_MODE=coverage && vendor/bin/phpunit tests/ --coverage-html var/cache/cover'
+scp -r /root/video-transcoder/develop/symfony/var/cache/cover 192.168.2.5:/mnt/seagate/mo/tmp
