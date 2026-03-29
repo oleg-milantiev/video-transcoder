b9735b759280decee1f28eebbcda385109e1ee4d
Oleg Milantiev <oleg@milantiev.com>
2026-03-29 20:03:55 +0300

    Default free tariff

--- develop/symfony/migrations/Version20260329092408.php
+++ develop/symfony/migrations/Version20260329092408.php
@@ -22,10 +22,16 @@ final class Version20260329092408 extends AbstractMigration
         $this->addSql('ALTER TABLE tariff ADD max_height INT NOT NULL DEFAULT 0');
         $this->addSql('ALTER TABLE tariff ADD storage_gb DOUBLE PRECISION NOT NULL DEFAULT 0');
         $this->addSql('ALTER TABLE tariff ADD storage_hour INT NOT NULL DEFAULT 0');
+
+        $this->addSql("INSERT INTO tariff (id, title, delay, instance, video_duration, video_size, max_width, max_height, storage_gb, storage_hour) VALUES ('905048e3-fd0f-408d-bffd-a596e896a92c', 'Free', 86400, 1, 3600, 100, 1920, 1280, 1, 24)");
+        $this->addSql("UPDATE \"user\" set tariff_id = '905048e3-fd0f-408d-bffd-a596e896a92c' WHERE email = 'oleg@milantiev.com'");
     }
 
     public function down(Schema $schema): void
     {
+        $this->addSql("UPDATE \"user\" set tariff_id = null WHERE email = 'oleg@milantiev.com'");
+        $this->addSql("DELETE FROM tariff WHERE id = '905048e3-fd0f-408d-bffd-a596e896a92c'");
+
         $this->addSql('ALTER TABLE tariff DROP video_duration');
         $this->addSql('ALTER TABLE tariff DROP video_size');
         $this->addSql('ALTER TABLE tariff DROP max_width');
