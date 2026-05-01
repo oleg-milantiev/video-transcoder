21a46999c61d2c8c13885552c123291397095993
Oleg Milantiev <oleg@milantiev.com>
2026-05-01 22:11:55 +0300

    Security - auth download poster and videos via X-Accel-Redirect   - cache

--- develop/symfony/src/Application/QueryHandler/GetStorageHandler.php
+++ develop/symfony/src/Application/QueryHandler/GetStorageHandler.php
@@ -8,6 +8,7 @@ use App\Domain\Shared\ValueObject\Uuid;
 use App\Domain\Video\Repository\VideoRepositoryInterface;
 use App\Infrastructure\Security\Voter\VideoAccessVoter;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Mime\MimeTypes;
@@ -18,6 +19,7 @@ final readonly class GetStorageHandler
     public function __construct(
         private VideoRepositoryInterface $videoRepository,
         private Security $security,
+        private AdapterInterface $cache,
     ) {
     }
 
@@ -32,14 +34,36 @@ final readonly class GetStorageHandler
             return new Response('Access denied', Response::HTTP_FORBIDDEN);
         }
 
-        $video = $this->videoRepository->findById($videoUuid);
+        $userUuid = $this->security->getUser()->id->toRfc4122();
+        $cacheKey = 'storage_access_'.(string)$videoUuid.'_'.$userUuid;
 
-        if ($video === null) {
-            return new Response('Access denied', Response::HTTP_FORBIDDEN);
-        }
+        // Check cache first
+        $cacheItem = $this->cache->getItem($cacheKey);
+        if ($cacheItem->isHit()) {
+            $isAllowed = $cacheItem->get();
+            if (!$isAllowed) {
+                return new Response('Access denied', Response::HTTP_FORBIDDEN);
+            }
+        } else {
+            $video = $this->videoRepository->findById($videoUuid);
+            $cacheItem->expiresAfter(3600 * 24); // 1 day
 
-        if (!$this->security->isGranted(VideoAccessVoter::CAN_DOWNLOAD, $video)) {
-            return new Response('Access denied', Response::HTTP_FORBIDDEN);
+            if ($video === null) {
+                $cacheItem->set(false);
+                $this->cache->save($cacheItem);
+
+                return new Response('Access denied', Response::HTTP_FORBIDDEN);
+            }
+
+            if (!$this->security->isGranted(VideoAccessVoter::CAN_DOWNLOAD, $video)) {
+                $cacheItem->set(false);
+                $this->cache->save($cacheItem);
+
+                return new Response('Access denied', Response::HTTP_FORBIDDEN);
+            }
+
+            $cacheItem->set(true);
+            $this->cache->save($cacheItem);
         }
 
         return new Response(null, Response::HTTP_OK, [
